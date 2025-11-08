272f0b61e4531817c3b1c6b0811ab2fa34e7b26a
lorenzolopesdiogo <lorenzolopesdiogo@gmail.com>
2025-11-08 06:27:26 -0300

    Update TCC.py

--- TCC.py
+++ TCC.py
@@ -1,1 +1,10 @@
-print ("seja bem vindo ao banco gremio")
+import json
+import os
+
+ARQUIVO = "contas.json" #nome do arquivo onde salva as contas 
+
+#carrega as contas do arquivo se existir 
+
+def carregar_contas():
+    if os.path.exists(ARQUIVO):    # vai verifica se o arquivo vai existir 
+        with open(ARQUIVO)
